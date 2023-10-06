66bc7ca404511f27a2848176825a7fa880a4baf1
morozov <morozov@ThinkBook_IAP>
2023-10-06 23:39:16 +0300

    The method setStudentID was added in StudentDatabaseApp class. This method increment inner id and after that generate studentID.

--- src/ua/com/studentdatabaseapp/Student.java
+++ src/ua/com/studentdatabaseapp/Student.java
@@ -6,10 +6,11 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
-	private int studentID;
+	private String studentID;
 	private String courses;
 	private int tuituonBalance;
-	private int costOfCourse = 600;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
 	
 	public Student() {
 		Scanner sc = new Scanner(System.in);
@@ -23,8 +24,18 @@ public class Student {
 		System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\n" + 
 				"Enter student class level: ");
 		this.gradeYear = sc.nextInt();
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
+		
 		
 		sc.close();
+		setStudentID();
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+		
+	}
+	
+	//Generate an ID
+	private void setStudentID() {
+		//Grade Level + ID
+		id++;
+		this.studentID = gradeYear + "" + id;
 	}
 }
