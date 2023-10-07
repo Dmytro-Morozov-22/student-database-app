e999514c8a2fedae52ce704c36e90a67719a22b1
morozov <morozov@ThinkBook_IAP>
2023-10-07 23:46:37 +0300

    The method enroll was added to the class Student and improvements to the constructor of this class have been made.

--- src/ua/com/studentdatabaseapp/Student.java
+++ src/ua/com/studentdatabaseapp/Student.java
@@ -7,29 +7,26 @@ public class Student {
 	private String lastName;
 	private int gradeYear;
 	private String studentID;
-	private String courses;
-	private int tuituonBalance;
+	private String courses = "Not enrolled to any";
+	private int tuituonBalance = 0;
 	private static int costOfCourse = 600;
 	private static int id = 1000;
 	
-	public Student() {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter student first name: ");
-		this.firstName = sc.nextLine();
-		
-		System.out.print("Enter student last name: ");
-		this.lastName = sc.nextLine();
-		
-		System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\n" + 
-				"Enter student class level: ");
-		this.gradeYear = sc.nextInt();
-		
-		
-		sc.close();
+	private Scanner sc = new Scanner(System.in);
+	
+	public Student() {		
+		this.firstName = addFieldValue("Enter student first name: ");
+		this.lastName = addFieldValue("Enter student last name: ");
+		this.gradeYear = Integer.valueOf(addFieldValue("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\n" + 
+				"Enter student class level: "));
 		setStudentID();
-		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
-		
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);	
+	}
+	
+	//Initializes fields from console
+	private String addFieldValue(String descriptionField) {
+		System.out.print(descriptionField);
+			return sc.nextLine();
 	}
 	
 	//Generate an ID
@@ -38,4 +35,31 @@ public class Student {
 		id++;
 		this.studentID = gradeYear + "" + id;
 	}
+	
+	//Enroll in courses
+	public void enroll() {
+		// Get inside a loop, user hits 0
+		do {
+			System.out.print("Enter course to enroll (Q to quit): ");
+			String course = sc.nextLine();
+			if(!course.equals("Q")) {
+				this.courses = courses.equals("Not enrolled to any") ? "\n" + course : courses + "\n" + course;
+				tuituonBalance = tuituonBalance + costOfCourse;	
+			} else {
+				break;
+			}	
+		} while(true);
+		
+		System.out.println("ENROLLED IN: " + courses);
+		System.out.println("TUITION BALANCE: " + tuituonBalance);
+		sc.close();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/ua/com/studentdatabaseapp/StudentDatabaseApp.java
+++ src/ua/com/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,6 +4,7 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		Student st = new Student();
+		st.enroll();
 
 	}
 
