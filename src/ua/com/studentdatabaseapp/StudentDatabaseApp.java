06209f33ef80aecd5cca7f9542fe4c2c9b9a879a
morozov <morozov@ThinkBook_IAP>
2023-10-09 22:10:46 +0300

    Added the ability to determine the number of students who will be enrolled in the database at the beginning of the program. This logic is in the main class

--- src/ua/com/studentdatabaseapp/StudentDatabaseApp.java
+++ src/ua/com/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,13 +1,29 @@
 package ua.com.studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		Student st = new Student();
-		st.enroll();
-		st.payTuition();
-		System.err.println(st.toString());
-
+		
+		System.out.print("Enter number of new students to enroll: ");
+		try(Scanner in = new Scanner(System.in)){
+		
+			int numberOfStudent = in.nextInt();
+			Student[] students = new Student[numberOfStudent];
+	
+			for(int i = 0; i < numberOfStudent; i++) {
+				students[i] = new Student();
+				students[i].enroll();
+				students[i].payTuition();
+				System.out.println();
+			}
+			
+			for (int k = 0; k < numberOfStudent; k++) 
+				System.out.println(students[k].toString());
+			
+		}
+		
 	}
 
 }
